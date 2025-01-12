2bf82ca413995e0a0879257caad518c4910b171d
Vlad Kashkarov <kwa-111@yandex.ru>
2025-01-12 21:31:38 +0300

    fix: убран вызов метода на авторизацию

--- src/test/java/api/BookApiSteps.java
+++ src/test/java/api/BookApiSteps.java
@@ -6,18 +6,18 @@ import models.book.IsbnBook;
 
 import java.util.List;
 
+import static helpers.LoginExtension.loginModel;
 import static io.restassured.RestAssured.given;
 import static specs.BaseSpec.*;
 
 public class BookApiSteps {
-
     @Step("Добавление книги в корзину")
     public BookApiSteps addBookApi(String ibsn){
         IsbnBook isbnModel = new IsbnBook(ibsn);
-        AddBookRequest request = new AddBookRequest(AuthData.getAuth().getUserId(), List.of(isbnModel));
+        AddBookRequest request = new AddBookRequest(loginModel.getUserId(), List.of(isbnModel));
 
         given(requestSpec)
-                .header("Authorization", "Bearer " + AuthData.getAuth().getToken())
+                .header("Authorization", "Bearer " + loginModel.getToken())
                 .body(request)
                 .when()
                 .post("/BookStore/v1/Books")
@@ -30,8 +30,8 @@ public class BookApiSteps {
     @Step("Удаление всех книг из корзины")
     public void deleteBookApi(){
         given(requestSpec)
-                .header("Authorization", "Bearer " + AuthData.getAuth().getToken())
-                .queryParams("UserId", AuthData.getAuth().getUserId())
+                .header("Authorization", "Bearer " + loginModel.getToken())
+                .queryParams("UserId", loginModel.getUserId())
                 .when()
                 .delete("/BookStore/v1/Books")
                 .then()
